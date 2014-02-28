9aa7899748d481b7caa040b515528eda1b85b2c3
John Kirk <jkirk777@gmail.com>
2014-02-28 01:25:36 -0700

    Emergency commit

--- lt02ltespr-vendor-blobs.mk
+++ lt02ltespr-vendor-blobs.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,3 +20,5 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/lt02ltespr/proprietary/bin/qmiproxy:system/bin/qmiproxy \
     vendor/samsung/lt02ltespr/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
     vendor/samsung/lt02ltespr/proprietary/lib/libaudioparsers.so:system/lib/libaudioparsers.so
+PRODUCT_COPY_FILES += \
+    vendor/samsung/lt02ltespr/proprietary/lib/libril.so:system/lib/libril.so
